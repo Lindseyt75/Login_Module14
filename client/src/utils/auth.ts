54da5feb7a641d9cd1294a0047dcd713c9e63655
Timothy Lindsey <lindseyt75@yahoo.com>
2025-02-20 13:47:32 -0500

    fixed auth routes

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,17 +1,10 @@
 import { jwtDecode, JwtPayload } from 'jwt-decode';
+import type { UserData } from '../interfaces/UserData';
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
-    const token = this.getToken();
-    if (token) {
-      try {
-        return jwtDecode<JwtPayload>(token);
-      } catch (e) {
-        return null;
-      }
-    }
-    return null;
+    return jwtDecode<UserData>(this.getToken());
   }
 
   loggedIn() {
@@ -19,34 +12,38 @@ class AuthService {
     const token = this.getToken();
     return !!token && !this.isTokenExpired(token);
   }
-  
+
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
     try {
       const decoded = jwtDecode<JwtPayload>(token);
-      return decoded.exp ? decoded.exp < Date.now() / 1000 : true;
-    } catch (e) {
-      return true;
+
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (err) {
+      return false;
     }
   }
   
   getToken(): string {
     // TODO: return the token
-    return localStorage.getItem('jwt_token') || '';
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
-    localStorage.setItem('jwt_token', idToken);
-    window.location.href = '/'; // Redirect to main Kanban board
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
-    localStorage.removeItem('jwt_token');
-    window.location.href = '/login'; // Redirect to login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
